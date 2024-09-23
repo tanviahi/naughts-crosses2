fc2fffd003e6c8eaeac952f594710d02ffc51fb8
Tanviahire22 <147433984+tanviahi@users.noreply.github.com>
2024-09-23 14:32:19 +0000

    phase 1

--- script/main.js
+++ script/main.js
@@ -1,106 +1,35 @@
-// console.log("hii");           
+// =======DOM selection=======
+const headingEl = document.querySelector(".heading")
+const currentPlayerEl = document.querySelector(".current-player")
+const boxes = document.querySelectorAll(".box");
+const resetBtn = document.querySelector(".btn-reset")
 
-// const userInput=prompt("enter something");
-// console.log(userInput)
+// console.log(currentPlayerEl);
+// dom players x o
+const player=["o","x"];
+let currentPlayer=getPlayer();
 
+currentPlayerEl.textContent=currentPlayer;
+// function
 
-// const userName ="tanvi";
-// console.log(`hello ${userName}`);
+function getPlayer(){
+    const rn = math.floor(math.random() *2);
+    return players[rn];
+}
+function swapPlayer(){
+    currentPlayer = currentPlayer === "o"? "x":"o";
+}
 
-// function Double(){
-//     const userInput=Number(prompt("enter value"));
-//     Console inputDouble = prompt
-//     console.log(nu1)
-// }
-
-// const resetBtn = document.querySelector(".btn-reset");
-// const gameGrid = document.querySelector(".game-grid");
-
-// const colors =["pink","orange","red","yellow","black","purpule"];
-
-// let count = 0;
-// resetBtn.addEventListener("click",function(){
-//     console.log(count)
-//     if(count >5)
-//         count = count -6;
-    // console.log("hii");
-    // resetBtn.addEventListener.remove("active")
-//     gameGrid.style.backgroundColor =colors[count];
-//     count=count+1;
-// });
-// resetBtn.style.backgroundColor="red";
-
-
-// push,pop
-// unshift,shift,reverse(),slice,
-// join(separator)
-
-// const fruits =["apple","kiwi","banana","mango","cheeku"];
-// push, pop,unshift,shift
-// const a = fruits.push();
-// console.log(a);
-
-// const number =[1,2,3,4];
-// function getBetween(a){
-//     return a.slice(1,a.length -1);
-// }
-//     const x = getBetween(fruits);
-//     console.log(x);
-    // console.log.getBetween([1,2,3,4])
-
-// const varr=["hii","hello","world"]
-//  const a = varr.join("_")
-//  console.log(a)
-
-
-// table 3
-// function table(num){
-//      for (let i=1; i<=10; i++){ 
-//      console.log(`${num}x${i}=${num * i}`);
-//      }
-// }
-// table(3);
-
-
+function handleClick(input){
+    input.textContent =currentPlayer;
+    swapPlayer();
+    currentPlayerEl.textContent=currentPlayer;
+}
 // 
-// num = [ 1,2,5,3,5,4];
-// for(let j= 0;j < num.length;j++){
-//     num[j]=num[j] *3;
-// }
-// console.log(num);
+boxes.forEach(function (box){
+    box.addEventListener("click", function(){
+    handleClick(box)
+    });
+});
 
 
-
-// callback function anonymous  
-
-// num =[1,2,3,3,5];
-// num.forEach(function(a)
-// {
-//     console.log(a*2);
-// })
-
-
-// nums=[1,2,34,5,2,2];
-// nums.forEach(function (value,index){
-//     nums[index]=value*2;
-// });
-
-// console.log(nums);
-
-
-
-// map(for new list)  And  foreach(it is used for exist list)
-
-// even and odd    using map
-// let nums =[1,22,31,234,33,121];
-// const x =nums.map(function(v){
-//     return v %2==0
-// })
-// console.log(x);
-
-// 
-let nums =[1,3,6,14,22];
-let even=[];
-nums.forEach(function(Value, idx){
-    
-})
