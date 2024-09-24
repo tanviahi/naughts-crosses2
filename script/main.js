8984e794453c17953f6b35cf0b6d8faf0068581e
Tanviahire22 <147433984+tanviahi@users.noreply.github.com>
2024-09-24 14:29:19 +0000

    checkjs done

--- script/main.js
+++ script/main.js
@@ -1,35 +1,64 @@
+// for import another js file  used(./)
+import chances from "./chances.js";
+
+console.log(chances);
 // =======DOM selection=======
-const headingEl = document.querySelector(".heading")
-const currentPlayerEl = document.querySelector(".current-player")
+const headingEl = document.querySelector(".heading");
+const currentPlayerEl = document.querySelector(".current-player");
 const boxes = document.querySelectorAll(".box");
-const resetBtn = document.querySelector(".btn-reset")
+const resetBtn = document.querySelector(".btn-reset");
 
 // console.log(currentPlayerEl);
 // dom players x o
-const player=["o","x"];
-let currentPlayer=getPlayer();
-
-currentPlayerEl.textContent=currentPlayer;
+const player = ["O", "X"];
+let currentPlayer;
+let virtualGrid;
+currentPlayerEl.textContent = currentPlayer;
 // function
 
-function getPlayer(){
-    const rn = math.floor(math.random() *2);
-    return players[rn];
+function getPlayer() {
+  const rn = Math.floor(Math.random() * 2);
+  return player[rn];
 }
-function swapPlayer(){
-    currentPlayer = currentPlayer === "o"? "x":"o";
+function swapPlayer() {
+  const newPlayer = currentPlayer === "O" ? "X" : "O";
+  currentPlayer = newPlayer;
+  currentPlayerEl.textContent = newPlayer;
 }
 
-function handleClick(input){
-    input.textContent =currentPlayer;
-    swapPlayer();
-    currentPlayerEl.textContent=currentPlayer;
+// =======start the game =======
+function startGame() {
+  // current player set random player
+  currentPlayer = getPlayer();
+  // virtual grid
+  virtualGrid = new Array(9).fill("");
+  currentPlayerEl.textContent = currentPlayer;
 }
-// 
-boxes.forEach(function (box){
-    box.addEventListener("click", function(){
-    handleClick(box)
-    });
+
+function handleBoxClick(input) {
+  if (virtualGrid[input] === "") {
+    console.log(input);
+    boxes[input].textContent = currentPlayer;
+    boxes[input].style.cursor = "auto";
+    virtualGrid[input] = currentPlayer;
+    console.log(virtualGrid);
+    swapPlayer();
+  }
+}
+console.log(boxes);
+
+boxes.forEach(function (_, index) {
+  _.addEventListener("click", function () {
+    handleBoxClick(index);
+  });
 });
 
+function handleReset() {
+  alert(1);
+}
+resetBtn.addEventListener("click", handleBoxClick);
+resetBtn.addEventListener("click", handleReset);
+//
 
+//
+document.addEventListener("DOMContentLoaded", startGame);
